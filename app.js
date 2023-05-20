c52624410f0c755af6ce27d3157e80a671443028
Elizabeth <Hindash@email.arizona.edu>
2023-05-19 21:06:48 -0700

    refactor ap.js file for more efficient code

--- app.js
+++ app.js
@@ -10,176 +10,171 @@ var config = {
   appId: "1:677879798054:web:af95ebc4fc5ef9e8a209e7"
 };
 firebase.initializeApp(config);
-var database = firebase.database();
+const database = firebase.database();
 
-var firebaseDataObject = null;
-//variable to store key of object to update.
-var updateKey;
-//variable to hold input values
-var name;
-var destination;
-var time;
-var frequency;
-// setup of train object
-function Train (name, destination, firstTrainTime, frequency)
-{
+let firebaseDataObject = null;
+let updateKey;
+let name;
+let destination;
+let time;
+let frequency;
+
+function Train(name, destination, firstTrainTime, frequency) {
   this.name = name;
   this.destination = destination;
   this.firstTrainTime = firstTrainTime;
   this.frequency = frequency;
 }
-$(document).ready(function ()
-{
-  $("#current-time").text(moment().format("MMM DD HH:mm A"));
-  setInterval(function ()
-  {
-    $("#current-time").text(moment().format("MMM DD HH:mm A"));
+
+$(document).ready(function() {
+  const $currentTime = $("#current-time");
+  $currentTime.text(moment().format("MMM DD HH:mm A"));
+
+  setInterval(function() {
+    $currentTime.text(moment().format("MMM DD HH:mm A"));
     displayTrainSchedule();
   }, 60000);
-  database.ref().on("value", function (data)
-  {
+
+  database.ref().on("value", function(data) {
     firebaseDataObject = data.val();
     displayTrainSchedule();
-  },
-    function (objectError)
-    {
-      console.log("error:" + objectError.code);
-    });
+  }, function(objectError) {
+    console.log("error:" + objectError.code);
+  });
 });
-$("#submit-btn").on("click", function (event)
-{
+
+$("#submit-btn").on("click", function(event) {
   event.preventDefault();
   if (getInputValues()) {
-    var firstTrainTime = firstTimeString(time);
-    var newTrain = new Train(name, destination, firstTrainTime, frequency);
+    const firstTrainTime = firstTimeString(time);
+    const newTrain = new Train(name, destination, firstTrainTime, frequency);
     database.ref().push(newTrain);
   }
 });
-// script to remove a train from schedule/
-$(document).on("click", ".remove", function ()
-{
-  var con = confirm("Are you sure you want to remove train?");
-  if (con == true) {
-    var key = $(this).attr("key");
+
+$(document).on("click", ".remove", function() {
+  const con = confirm("Are you sure you want to remove the train?");
+  if (con) {
+    const key = $(this).attr("key");
     database.ref().child(key).remove();
   }
 });
-//Script to update trains schedule/
-$(document).on("click", ".update", function ()
-{
+
+$(document).on("click", ".update", function() {
   updateKey = $(this).attr("key");
   displayUpdate();
 });
-$("#close-btn").on("click", function (event)
-{
+
+$("#close-btn").on("click", function(event) {
   event.preventDefault();
   updateDone();
 });
-$("#update-btn").on("click", function (event)
-{
+
+$("#update-btn").on("click", function(event) {
   event.preventDefault();
   updateTrain();
 });
-//Script to add train/
-$("#add-train-btn").on("click", function (event)
-{
+
+$("#add-train-btn").on("click", function(event) {
   event.preventDefault();
   $("#submit-btn").css("display", "initial");
   $("#add-panel").slideToggle();
 });
-// function for next arrival time/
-function getNextArrival (time, frequency)
-{
-  var nextArrival = moment(time);
+
+function getNextArrival(time, frequency) {
+  let nextArrival = moment(time);
   while (nextArrival < moment()) {
     nextArrival.add(frequency, "minutes");
   }
   return nextArrival;
 }
-// Calculates and returns how many minutes away next train is.
-function getMinutesAway (time)
-{
-  var minutesAway = Math.round(getNextArrival(time).diff(moment(), "minutes", true));
-  return (minutesAway === 0) ? "Arrived" : minutesAway
+
+function getMinutesAway(time) {
+  const minutesAway = Math.round(getNextArrival(time).diff(moment(), "minutes", true));
+  return (minutesAway === 0) ? "Arrived" : minutesAway;
 }
-//Gets trains data from database, then used data to displays train stats on screen.
-function displayTrainSchedule ()
-{
-  $("#schedule").empty();
+
+function displayTrainSchedule() {
+  const $schedule = $("#schedule");
+  $schedule.empty();
+
   if (firebaseDataObject !== null) {
-    Object.keys(firebaseDataObject).forEach(function (key)
-    {
-      var name = firebaseDataObject[key].name;
-      var destination = firebaseDataObject[key].destination;
-      var firstTrainTime = firebaseDataObject[key].firstTrainTime;
-      var frequency = firebaseDataObject[key].frequency;
-      var nextArrival = getNextArrival(firstTrainTime, frequency);
-      var minutesAway = getMinutesAway(nextArrival);
-      var newTableRow = $("<tr>");
+    Object.keys(firebaseDataObject).forEach(function(key) {
+      const name = firebaseDataObject[key].name;
+      const destination = firebaseDataObject[key].destination;
+      const firstTrainTime = firebaseDataObject[key].firstTrainTime;
+      const frequency = firebaseDataObject[key].frequency;
+      const nextArrival = getNextArrival(firstTrainTime, frequency);
+      const minutesAway = getMinutesAway(nextArrival);
+      const newTableRow = $("<tr>");
       newTableRow.append($("<td>").html(name));
       newTableRow.append($("<td>").html(destination));
       newTableRow.append($("<td>").html(frequency));
       newTableRow.append($("<td>").html(nextArrival.format("MMM DD hh:mm A")));
       newTableRow.append($("<td>").html(minutesAway));
-      var newDiv = $("<div>") //$("<button>")
-      newDiv.addClass("update");
-      newDiv.attr(
-        {
+
+      const updateButton = $("<div>")
+        .addClass("update")
+        .attr({
           "key": key,
           "data-toggle": "tooltip",
           "data-placement": "left",
           "title": "Update"
-        });
-      newDiv.html("<span class='far fa-edit'></span>");
-      newTableRow.append($("<td>").html(newDiv));
-      var newDiv = $("<div>") //$("<button>")
-      newDiv.addClass("remove");
-      newDiv.attr(
-        {
+        })
+        .html("<span class='far fa-edit'></span>");
+      newTableRow.append($("<td>").html(updateButton));
+
+      const removeButton = $("<div>")
+        .addClass("remove")
+        .attr({
           "key": key,
           "data-toggle": "tooltip",
           "data-placement": "left",
           "title": "Remove"
-        });
-      newDiv.html("<span class='fas fa-trash-alt'></span>");
-      newTableRow.append($("<td>").html(newDiv));
-      $("#schedule").append(newTableRow);
+        })
+        .html("<span class='fas fa-trash-alt'></span>");
+      newTableRow.append($("<td>").html(removeButton));
+
+      $schedule.append(newTableRow);
     });
   }
 }
-function firstTimeString (time)
-{
-  var currentDateString = moment().format("YYYY-MM-DD");
-  return (currentDateString + "T" + time);
+
+function firstTimeString(time) {
+  const currentDateString = moment().format("YYYY-MM-DD");
+  return currentDateString + "T" + time;
 }
-//Pads time if hour or minute is single digit (ex 9:25 becomes 09:25)
-function pad (time)
-{
-  var array = time.split(":");
-  array[0] = parseInt(array[0]); //HH
-  array[1] = parseInt(array[1]); //MM
+
+function pad(time) {
+  const array = time.split(":");
+  array[0] = parseInt(array[0]);
+  array[1] = parseInt(array[1]);
+
   if (array[0] < 10) {
     array[0] = '0' + array[0];
   }
+
   if (array[1] < 10) {
     array[1] = '0' + array[1];
   }
-  return (array[0] + ":" + array[1]);
+
+  return array[0] + ":" + array[1];
 }
-// checks if time entered is valid
-function checkTime (time)
-{
-  var array = time.split(":");
-  if ((isNaN(array[0])) || (isNaN(array[1]))) {
+
+function checkTime(time) {
+  const array = time.split(":");
+
+  if (isNaN(array[0]) || isNaN(array[1])) {
     return false;
   }
+
   array[0] = parseInt(array[0]);
   array[1] = parseInt(array[1]);
-  return ((array[0] >= 0 && array[0] <= 23) && (array[1] >= 0 && array[1] <= 59)) ? true : false;
+
+  return (array[0] >= 0 && array[0] <= 23) && (array[1] >= 0 && array[1] <= 59);
 }
-//Populates input fields with current train data to update.
-function displayUpdate ()
-{
+
+function displayUpdate() {
   $("#add-panel").slideDown();
   $("#submit-btn").css("display", "none");
   $("#update-btn").css("display", "initial");
@@ -189,10 +184,8 @@ function displayUpdate ()
   $("#time").val(moment(firebaseDataObject[updateKey].firstTrainTime).format("HH:mm"));
   $("#frequency").val(firebaseDataObject[updateKey].frequency);
 }
-//Clears out values in input boxes
-////Changes panel title to back to 'Add Train'.
-function updateDone ()
-{
+
+function updateDone() {
   $("#name").val("");
   $("#destination").val("");
   $("#time").val("");
@@ -202,50 +195,40 @@ function updateDone ()
   $("#submit-btn").css("display", "initial");
   $("#update-btn").css("display", "none");
 }
-//Updates train data to firebase.
-function updateTrain ()
-{
+
+function updateTrain() {
   if (getInputValues()) {
-    var firstTrainTime = firstTimeString(time);
-    var newTrain = new Train(name, destination, firstTrainTime, frequency);
+    const firstTrainTime = firstTimeString(time);
+    const newTrain = new Train(name, destination, firstTrainTime, frequency);
     database.ref("/" + updateKey).update(newTrain);
     updateDone();
   }
 }
-// Gets input values from user on page and check for validity.
-// If all values are valid, 'true' is returned; 'false' otherwise.
-function getInputValues ()
-{
+
+function getInputValues() {
   name = $("#name").val().trim();
   destination = $("#destination").val().trim();
-  time = $("#time").val().trim().replace(/\s/g, ""); //uses regexp to remove all white space
+  time = $("#time").val().trim().replace(/\s/g, "");
   frequency = parseInt($("#frequency").val().trim());
-  //Tests if 'Train Name' value is empty.
+
   if (name === "") {
-    alert("Please Enter A Train Name");
+    alert("Please enter a train name.");
     $("#name").val("").focus();
     return false;
-  }
-  //Tests if 'Destination' value is empty.
-  else if (destination === "") {
-    alert("Please Enter A Destination");
+  } else if (destination === "") {
+    alert("Please enter a destination.");
     $("#destination").val("").focus();
     return false;
-  }
-  else if (!checkTime(time)) {
-    alert("Please Enter A Valid First Train Time! (HH:MM)");
+  } else if (!checkTime(time)) {
+    alert("Please enter a valid first train time (HH:MM).");
     $("#time").val("").focus();
     return false;
-  }
-  else if (isNaN(frequency)) {
-    alert("Please Enter A Frequency");
+  } else if (isNaN(frequency)) {
+    alert("Please enter a frequency.");
     $("#frequency").val("").focus();
     return false;
-  }
-  else {
-    // assist with single digit time
+  } else {
     time = pad(time);
-    //Clears out input box fields
     $("#name").val("");
     $("#destination").val("");
     $("#time").val("");
@@ -253,3 +236,6 @@ function getInputValues ()
     return true;
   }
 }
+
+
+
